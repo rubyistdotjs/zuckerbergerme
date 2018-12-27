4173bab47ca3e21f3bb0095a629b1b35281fddf3
rubyistdotjs <rubyistdotjs@gmail.com>
2018-12-27 14:02:23 +0100

    Update Dropzone

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -20,21 +20,25 @@ class ImageUpload extends React.PureComponent {
         multiple={false}
         disableClick={true}
         onDrop={this.handleDrop}
-        className="flex-1 flex flex-col justify-center items-center text-center w-full h-full p-8 md:p-12 border-2 border-grey-light border-dashed rounded"
-        activeClassName="border-purple-light"
       >
-        {({ open }) => (
-          <React.Fragment>
+        {({ getRootProps, getInputProps, isDragActive, open }) => (
+          <div
+            {...getRootProps()}
+            className={`${
+              isDragActive ? 'border-purple-light' : ''
+            } flex-1 flex flex-col justify-center items-center text-center w-full h-full p-8 md:p-12 border-2 border-grey-light border-dashed rounded`}
+          >
             <h1 className="text-black text-2xl font-semibold leading-tight antialiased mb-2">
               Drag & Drop your picture here
             </h1>
             <span className="text-grey font-semibold leading-tight">
               Only .png, .jpg or .jpeg
             </span>
+            <input {...getInputProps()} />
             <button onClick={() => open()} className="btn btn-purple mt-8">
               Select a picture
             </button>
-          </React.Fragment>
+          </div>
         )}
       </Dropzone>
     );
