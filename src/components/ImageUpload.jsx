f528b7e2504456ab1edf50fa8c186df40083d0bd
rubyistdotjs <rubyistdotjs@gmail.com>
2019-12-31 13:13:18 +0100

    Update react-dropzone

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,43 +1,38 @@
 import React, { useCallback } from 'react';
-import Dropzone from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 
 function ImageUpload({ onImageChange }) {
-  const acceptedMimeTypes = ['image/png', 'image/jpeg'];
-
-  const handleDrop = useCallback(
-    (acceptedFiles, rejectedFiles) => {
+  const onDrop = useCallback(
+    acceptedFiles => {
       onImageChange(acceptedFiles[0]);
     },
     [onImageChange]
   );
 
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    accept: ['image/png', 'image/jpeg'],
+    multiple: false,
+    onDrop,
+  });
+
   return (
-    <Dropzone
-      accept={acceptedMimeTypes}
-      multiple={false}
-      disableClick={true}
-      onDrop={handleDrop}
+    <div
+      {...getRootProps()}
+      className={`${
+        isDragActive ? 'border-purple-light' : ''
+      } flex-1 flex flex-col justify-center items-center text-center w-full h-full p-8 md:p-12 border-2 border-grey-light border-dashed rounded`}
     >
-      {({ getRootProps, getInputProps, isDragActive, open }) => (
-        <div
-          {...getRootProps()}
-          className={`${
-            isDragActive ? 'border-purple-light' : ''
-          } flex-1 flex flex-col justify-center items-center text-center w-full h-full p-8 md:p-12 border-2 border-grey-light border-dashed rounded`}
-        >
-          <h1 className="text-black text-2xl font-semibold leading-tight antialiased mb-2">
-            Drag & Drop your picture here
-          </h1>
-          <span className="text-grey font-semibold leading-tight">
-            Only .png, .jpg or .jpeg
-          </span>
-          <input {...getInputProps()} />
-          <button onClick={() => open()} className="btn btn-purple mt-8">
-            Select a picture
-          </button>
-        </div>
-      )}
-    </Dropzone>
+      <h1 className="text-black text-2xl font-semibold leading-tight antialiased mb-2">
+        Drag & Drop your picture here
+      </h1>
+      <span className="text-grey font-semibold leading-tight">
+        Only .png, .jpg or .jpeg
+      </span>
+      <input {...getInputProps()} />
+      <button onClick={open} className="btn btn-purple mt-8">
+        Select a picture
+      </button>
+    </div>
   );
 }
 
